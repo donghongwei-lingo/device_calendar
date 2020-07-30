41bb918f78c3b08386259a7723c72fb7a9c624f7
shanmingming <978874646@qq.com>
2020-07-30 14:24:18 +0800

    增加安卓删除日历提醒

--- android/src/main/kotlin/com/builttoroam/devicecalendar/DeviceCalendarPlugin.kt
+++ android/src/main/kotlin/com/builttoroam/devicecalendar/DeviceCalendarPlugin.kt
@@ -23,10 +23,12 @@ class DeviceCalendarPlugin() : MethodCallHandler {
     private val DELETE_EVENT_INSTANCE_METHOD = "deleteEventInstance"
     private val CREATE_OR_UPDATE_EVENT_METHOD = "createOrUpdateEvent"
     private val CREATE_CALENDAR_METHOD = "createCalendar"
+    private val DELETE_CALENDAR_MATCH_DESCRIPTION_METHOD = "deleteDefaultCalendarMatchDescriptionEvent"
 
     // Method arguments
     private val CALENDAR_ID_ARGUMENT = "calendarId"
     private val CALENDAR_NAME_ARGUMENT = "calendarName"
+    private val CALENDAR_DESCRIPTION_ARGUMENT = "description"
     private val START_DATE_ARGUMENT = "startDate"
     private val END_DATE_ARGUMENT = "endDate"
     private val EVENT_IDS_ARGUMENT = "eventIds"
@@ -114,6 +116,14 @@ class DeviceCalendarPlugin() : MethodCallHandler {
 
                 _calendarDelegate.deleteEvent(calendarId!!, eventId!!, result)
             }
+            DELETE_CALENDAR_MATCH_DESCRIPTION_METHOD -> {
+                val calendarId = call.argument<String>(CALENDAR_ID_ARGUMENT)
+                val eventId = call.argument<String>(EVENT_ID_ARGUMENT)
+                val description = call.argument<String>(CALENDAR_DESCRIPTION_ARGUMENT)
+                if (description != null) {
+                    _calendarDelegate.deleteEventByDescription(description,result)
+                }
+            }
             DELETE_EVENT_INSTANCE_METHOD -> {
                 val calendarId = call.argument<String>(CALENDAR_ID_ARGUMENT)
                 val eventId = call.argument<String>(EVENT_ID_ARGUMENT)
@@ -123,6 +133,7 @@ class DeviceCalendarPlugin() : MethodCallHandler {
 
                 _calendarDelegate.deleteEvent(calendarId!!, eventId!!, result, startDate, endDate, followingInstances)
             }
+
             CREATE_CALENDAR_METHOD -> {
                 val calendarName = call.argument<String>(CALENDAR_NAME_ARGUMENT)
                 val calendarColor = call.argument<String>(CALENDAR_COLOR_ARGUMENT)
